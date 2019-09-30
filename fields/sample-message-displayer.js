9b0403389e31871d0ecba708133668f70abed9d7
Michael Whitman <michael.whitman@cloudcms.com>
2019-09-30 13:55:50 -0400

    just the one field

---
+++ fields/sample-message-displayer.js
@@ -0,0 +1,93 @@
+define(function (require, exports, module) {
+
+
+
+    var Alpaca = require("alpaca");
+
+
+
+    Alpaca.Fields.ResourceField = Alpaca.Fields.ObjectField.extend({
+
+
+
+        getFieldType: function () {
+
+            return "custom-oup-sp-searchengine";
+
+        },
+
+
+
+        setup: function () {
+
+            this.base();
+
+
+
+            this.schema = {
+
+                "properties": {
+
+                    "showInSearchEngns": {
+
+                        "type": "string",
+
+                        "title": "Hide this page from search engines",
+
+                        "enum": [
+
+                            "Yes",
+
+                            "No"
+
+                        ],
+
+                        "default": "No"
+
+                    }
+
+                }
+
+            };
+
+
+
+            Alpaca.merge(this.options, {
+
+                "fields": {
+
+                    "showInSearchEngns": {
+
+                        "type": "select",
+
+                        "removeDefaultNone": true
+
+                    }
+
+                }
+
+            });
+
+        },
+
+
+
+        afterRenderContainer: function (model, callback) {
+
+            var self = this;
+
+            self.displayMessage({ "id": "showInSearchEngns", "message": "WARNING!" });
+
+            callback();
+
+        }
+
+    });
+
+
+
+    Alpaca.registerFieldClass("sample-message-displayer", Alpaca.Fields.ResourceField);
+
+
+
+});
