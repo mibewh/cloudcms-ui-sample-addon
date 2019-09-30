b9f7af1cf4d169833765957595af4b4cb0198b27
Michael Whitman <michael.whitman@cloudcms.com>
2019-09-30 14:31:30 -0400

    again

--- fields/sample-message-displayer.js
+++ fields/sample-message-displayer.js
@@ -24,7 +24,7 @@ define(function (require, exports, module) {
 
 
 
-            this.schema = {
+            Alpaca.merge(this.schema, {
 
                 "properties": {
 
@@ -48,7 +48,7 @@ define(function (require, exports, module) {
 
                 }
 
-            };
+            });
 
 
 
