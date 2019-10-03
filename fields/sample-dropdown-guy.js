09e8be9168b925586a5bbc21a5e7ffa9b9fac4e8
Michael Whitman <michael.whitman@cloudcms.com>
2019-10-03 09:42:17 -0400

    woo

--- fields/sample-dropdown-guy.js
+++ fields/sample-dropdown-guy.js
@@ -15,8 +15,11 @@ define(function (require, exports, module) {
    
             this.base(data);
             debugger;
-            $(self.getFieldEl()).remove("#warning-message");
-            $(self.getFieldEl()).append("<span id='warning-message'>WARNING!</span>");
+            $(self.getFieldEl()).remove(".warning-message");
+            if (data[0] == 'Yes')
+            {
+                $(self.getFieldEl()).append("<span class='warning-message'>WARNING!</span>");
+            }
 
             // callback();
 
